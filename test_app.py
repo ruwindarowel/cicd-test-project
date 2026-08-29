78cd58f6db10a036cfb6f095e0479d4d4548ee2b
ruwindarowel <ruwirowe@gmail.com>
2026-08-29 18:57:45 +0530

    Updated Test Bug

--- test_app.py
+++ test_app.py
@@ -1,7 +1,11 @@
+from fastapi.testclient import TestClient
+
 from app import app
 
+client = TestClient(app)
+
 def test_home():
-    response=app.test_client().get("/")
-    
-    assert response.status_code==200
-    assert response.data == b"Hello World"
+    response = client.get("/hello-world/")
+
+    assert response.status_code == 200
+    assert response.json() == "Hello World"
